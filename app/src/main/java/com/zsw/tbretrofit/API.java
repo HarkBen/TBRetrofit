fd9ee6cd2285d613b703e6aa58f4716e878f1999
zhusw <sink50@foxmail.com>
2016-10-11 14:36:50 +0800

    Initial commit

---
+++ app/src/main/java/com/zsw/tbretrofit/API.java
@@ -0,0 +1,22 @@
+package com.zsw.tbretrofit;
+
+
+
+/**
+ * Create on 2016/9/22.
+ * github  https://github.com/HarkBen
+ * Description:
+ * -----------
+ * author Ben
+ * Last_Update - 2016/9/22
+ */
+
+public class API {
+
+    public static final String BASEURL ="http://192.168.0.229:8080/";
+
+    public static final String UPLOADFILE = "http://192.168.0.229:8080/upload";
+
+    public static final String LOGINTOBR = "http://192.168.0.229:8080/login";
+
+}
---
+++ app/src/main/java/com/zsw/tbretrofit/CustomAPL.java
@@ -0,0 +1,32 @@
+package com.zsw.tbretrofit;
+
+import android.app.Application;
+
+import com.tb.tbretrofit.httputils.factory.TBOkHttpClientFactory;
+import com.tb.tbretrofit.httputils.factory.TBRequestFactory;
+import com.tb.tbretrofit.httputils.factory.TBRetrofitFactory;
+
+/**
+ * Create on 2016/10/11.
+ * github  https://github.com/HarkBen
+ * Description:
+ * -----------
+ * author Ben
+ * Last_Update - 2016/10/11
+ */
+public class CustomAPL extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        TBOkHttpClientFactory.Builder.create()
+                .setDebug(true)
+                .syncCookie(this)
+                .setTimeout_connection(10)
+                .setTimeout_read(10)
+                .setTimeout_write(10)
+                .build();
+        TBRequestFactory.build(TBRetrofitFactory.getInstance("http://www.xx.com/"));
+
+    }
+}
