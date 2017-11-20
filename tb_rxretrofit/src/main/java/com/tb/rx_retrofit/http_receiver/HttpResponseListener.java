3c91d11a452d5af59e85bdf2425d64bd0818a333
rango <zhushiwen@feixiong.tv>
2017-11-20 10:45:35 +0800

    -补全遗失文件

---
+++ tb_rxretrofit/src/main/java/com/tb/rx_retrofit/http_receiver/HttpResponseListener.java
@@ -0,0 +1,30 @@
+package com.tb.rx_retrofit.http_receiver;
+
+import android.content.Context;
+
+import com.tb.rx_retrofit.tools.CacheModel;
+
+import retrofit2.Response;
+
+/**
+ * @描述： -
+ * -
+ * @作者：zhusw
+ * @创建时间：17/11/16 上午11:13
+ * @最后更新时间：17/11/16 上午11:13
+ */
+public interface HttpResponseListener {
+
+    void onStart();
+
+    void onResponse (Response<String> response);
+
+    void onFailure (int errorCode, String message);
+
+    void onFinish ();
+
+    Context getContext ();
+
+    CacheModel cacheModel ();
+
+}
